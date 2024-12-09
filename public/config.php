f5084bd20577760b1357a0d8a5de6567cdcdf2f2
Pol González Miranda <pgmxx04@gmail.com>
2024-12-09 20:58:14 +0100

    Errores corregidos

--- public/config.php
+++ public/config.php
@@ -3,8 +3,8 @@ $config = [
     'db' => [
         'connection' => 'mysql:host=localhost',
         'dbname' => 'subhasta',
-        'usr' => 'root',
-        'pwd' => '1234',
+        'usr' => 'subhastador',
+        'pwd' => 'C@pernic1234',
         'options' => [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
